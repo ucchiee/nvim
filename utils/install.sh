bf37351d963e47406cfbb0dc93bd912c522211b7
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-04-24 11:36:31 +0900

    install.sh: add comment

--- utils/install.sh
+++ utils/install.sh
@@ -1,13 +1,16 @@
+# Check whether nvim is installed.
 if !(type nvim > /dev/null 2>&1); then
   echo "Neovim is not installed, or 'nvim' is not in the PATH."
   exit 1
 fi
 
+# Install vim-plug
 if [ -e ~/.config/nvim/autoload.plug.vim ]; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
+# Install vim plugin
 echo "Installing Neovim-plugin."
 nvim -c PlugInstall -c qall
 exit 0

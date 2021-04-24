85fc6e16c7c24ce9b86c7be4734f9c02b90779d8
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-04-24 23:28:26 +0900

    install.sh: overall installer

--- utils/install.sh
+++ utils/install.sh
@@ -1,15 +1,106 @@
 #!/bin/sh
 
-set -o nounset # error when referencing undefined variable
-set -o errexit # exit when command fails
+set -o nounset
+set -o errexit
 
-# Check whether nvim is installed.
-if !(type nvim > /dev/null 2>&1); then
-  echo "Neovim is not installed, or 'nvim' is not in the PATH."
-  exit 1
+installpip() {
+  # install pyenv? pipenv?
+  echo "Install python3 first."
+  echo "There're various ways to install python. i.e original, pyenv..."
+  echo "You may wanna chose your favorite one."
+  exit
+}
+
+install_node_ubuntu() {
+  sudo apt install nodejs
+  sudo apt install npm
+}
+
+install_node_mac() {
+  brew install node
+  brew install yarn
+}
+
+installnode() {
+	echo "Installing node..."
+	[ "$(uname)" == "Darwin" ] && install_node_mac
+	[ -n "$(uname -a | grep Ubuntu)" ] && install_node_ubuntu
+  [ -f "/etc/arch-release" ] && echo "Arch not currently supported"
+  [ -f "/etc/fedora-release" ] && "Fedora not currently supported"
+	[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] && echo "Windows not currently supported"
+	sudo npm i -g neovim
+}
+
+asktoinstallnode() {
+	echo "node not found"
+	echo -n "Would you like to install node now (y/n)? "
+	read answer
+	[ "$answer" != "${answer#[Yy]}" ] && installnode
+}
+
+install_nvim_mac() {
+  brew install neovim
+}
+
+install_nvim_ubuntu() {
+  sudo apt install neovim
+  # sudo apt install python-neovim
+  # sudo apt install python3-neovim
+}
+
+installnvim() {
+	echo "Installing nvim..."
+	[ "$(uname)" == "Darwin" ] && install_nvim_mac
+	[ -n "$(uname -a | grep Ubuntu)" ] && install_nvim_ubuntu
+  [ -f "/etc/arch-release" ] && echo "Arch not currently supported"
+  [ -f "/etc/fedora-release" ] && "Fedora not currently supported"
+	[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] && echo "Windows not currently supported"
+	sudo npm i -g neovim
+}
+
+asktoinstallnvim() {
+	echo "nvim not found"
+	echo -n "Would you like to install nvim now (y/n)? "
+	read answer
+	[ "$answer" != "${answer#[Yy]}" ] && installnvim
+}
+
+asktoinstallnode() {
+	echo "nvim not found"
+	echo -n "Would you like to install nvim now (y/n)? "
+	read answer
+	[ "$answer" != "${answer#[Yy]}" ] && installnvim
+}
+
+installpynvim() {
+  echo "Installing pynvim"
+  pip3 install pynvim
+}
+
+clone_config() {
+  echo "cloning config files"
+  git clone https://github.com/tomato-300yen/nvim.git ~/.config/nvim
+  echo "Installing Neovim-plugin."
+  nvim -c PlugInstall -c qall
+}
+
+# check pip installed
+which pip3 >/dev/null && echo "pip installed, moving on..." || installpip
+
+# install nvim
+which nvim >/dev/null && echo "nvim installed, moving on..." || asktoinstallnvim
+
+# install node and neovim support
+which node >/dev/null && echo "node installed, moving on..." || asktoinstallnode
+
+# install pynvim
+pip3 list | grep pynvim >/dev/null && echo "pynvim installed, moving on..." || installpynvim
+
+# clone config
+if [ -a "$HOME/.config/nvim/init.vim" ]; then
+  echo "There's already your init.vim"
+  echo "Updating plugins"
+  nvim -c PlugInstall -c qall
+else
+  clone_config
 fi
-
-# Install vim plugin
-echo "Installing Neovim-plugin."
-nvim -c PlugInstall -c qall
-exit 0

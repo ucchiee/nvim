7617aaa8af9efa64483596f1f426e967280e2f0e
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-04-24 11:29:00 +0900

    install.sh: add branch cond

--- utils/install.sh
+++ utils/install.sh
@@ -1,3 +1,13 @@
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim -c PlugInstall -c qall
+if [ -e ~/.config/nvim/autoload.plug.vim ]; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+if type nvim > /dev/null 2>&1; then
+  echo "Installing Neovim-plugin."
+  nvim -c PlugInstall -c qall
+else
+  echo "Neovim is not installed, or 'nvim' is not in the PATH."
+  exit 1
+fi
+
+exit 0

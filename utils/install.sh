af1fb3dc2d42e071acf5ccf73b6adbc2f952c1b4
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-04-24 11:33:13 +0900

    install.sh: changed structure

--- utils/install.sh
+++ utils/install.sh
@@ -1,13 +1,13 @@
-if [ -e ~/.config/nvim/autoload.plug.vim ]; then
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-fi
-if type nvim > /dev/null 2>&1; then
-  echo "Installing Neovim-plugin."
-  nvim -c PlugInstall -c qall
-else
+if !(type nvim > /dev/null 2>&1); then
   echo "Neovim is not installed, or 'nvim' is not in the PATH."
   exit 1
 fi
 
+if [ -e ~/.config/nvim/autoload.plug.vim ]; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
+echo "Installing Neovim-plugin."
+nvim -c PlugInstall -c qall
 exit 0

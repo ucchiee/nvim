579c784f44077b1c3dfe2435c0e1c50093e801b6
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-22 12:53:13 +0900

    plug-lualine.lua: configured diff settings

--- lua/plug-lualine.lua
+++ lua/plug-lualine.lua
@@ -1,3 +1,28 @@
+local conditions = {
+  buffer_not_empty = function() return vim.fn.empty(vim.fn.expand('%:t')) ~= 1 end,
+  hide_in_width = function() return vim.fn.winwidth(0) > 80 end,
+  check_git_workspace = function()
+    local filepath = vim.fn.expand('%:p:h')
+    local gitdir = vim.fn.finddir('.git', filepath .. ';')
+    return gitdir and #gitdir > 0 and #gitdir < #filepath
+  end
+}
+
+-- almost onedark
+local colors = {
+  bg = '#202328',
+  fg = '#bbc2cf',
+  yellow = '#f3f99d',
+  cyan = '#9aedfe',
+  darkblue = '#081633',
+  green = '#5af78e',
+  orange = '#FF8800',  -- this is not from orange
+  violet = '#C678DD',
+  magenta = '#ff6ac1',
+  blue = '#57c7ff',
+  red = '#ff5c57'
+}
+
 require'lualine'.setup {
   options = {
     icons_enabled = true,
@@ -9,7 +34,18 @@ require'lualine'.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
-    lualine_c = {'filename', 'diff'},
+    lualine_c = {
+      'filename',
+      {
+        'diff',
+        -- Is it me or the symbol for modified us really weird
+        symbols = {added = ' ', modified = '柳 ', removed = ' '},
+        color_added = colors.green,
+        color_modified = colors.orange,
+        color_removed = colors.red,
+        condition = conditions.hide_in_width
+      }
+    },
     lualine_x = {{ 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} }, 'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}

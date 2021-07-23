28e46990d174a45500a27256629cf83470b7c873
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-23 18:00:04 +0900

    plug-treesitter.lua: disabled ts-rainbow

--- lua/plug-treesitter.lua
+++ lua/plug-treesitter.lua
@@ -10,13 +10,13 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-    colors = {}, -- table of hex strings
-    termcolors = {} -- table of colour name strings
-  },
+  -- rainbow = {
+  --   enable = true,
+  --   extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+  --   max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+  --   colors = {}, -- table of hex strings
+  --   termcolors = {} -- table of colour name strings
+  -- },
   context_commentstring = {
     enable = true
   },

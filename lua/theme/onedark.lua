6be160741728440a9c9842341505f4de0bd68bb3
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-26 21:55:12 +0900

    onedark.lua: init

---
+++ lua/theme/onedark.lua
@@ -0,0 +1,5 @@
+vim.g.onedark_transparent_background = true
+vim.g.onedark_italic_comment = true
+vim.g.onedark_italic_comment = true  -- <leader>cs
+-- vim.g.onedark_style = 'warmer'
+require('onedark').setup()

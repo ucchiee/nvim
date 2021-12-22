9b7d0cce227fe29578f47b26c6b61de5dcc3b1bc
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 23:18:49 +0900

    plugins.lua: installed telescope and related plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -67,6 +67,10 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

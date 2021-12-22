a44766788d4390bd5433ad6a0c0f312133e4692f
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 23:18:56 +0900

    init.lua: configured telescope

--- init.lua
+++ init.lua
@@ -4,3 +4,4 @@ require "user.plugins"  -- source lua/user/plugins.lua
 require "user.colorscheme"  -- source lua/user/colorscheme.lua
 require "user.cmp"  -- source lua/user/cmp.lua
 require "user.lsp"  -- source lua/user/lsp/init.lua
+require "user.telescope"  -- source lua/user/telescope.lua

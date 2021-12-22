149136a82ab83a79bb63c25d1d6fa8869faf9a28
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 22:18:35 +0900

    plugins.lua: added cmp-nvim-lsp, nvim-lspconfig, nvim-lsp-installer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,11 +56,16 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "hrsh7th/cmp-nvim-lua" -- nvim api completion
+  use "hrsh7th/cmp-nvim-lsp" -- lsp
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins

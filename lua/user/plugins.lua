e4d198cf13aa566299a98e1dbae04f0d9326c10f
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 23:52:23 +0900

    plugins.lua: installed vim-floaterm

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,6 +71,10 @@ return packer.startup(function(use)
   use "nvim-telescope/telescope.nvim"
   use 'nvim-telescope/telescope-media-files.nvim'
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  -- FTerm
+  use 'voldikss/vim-floaterm'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

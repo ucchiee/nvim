921fb8f3fd402a36c630af0ff0d8bbee3e222d7f
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 11:07:04 +0900

    options.lua: enabled netrw

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,3 +44,7 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+vim.cmd [[
+	set nocp                    " 'compatible' is not set
+	filetype plugin on          " plugins are enabled
+]]

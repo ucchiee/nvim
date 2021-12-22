d977f9bc2df25efe0695de67c1ad523ae9c403e6
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 11:06:40 +0900

    keymaps.lua: init

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,76 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<M-j>", ":resize -2<CR>", opts)
+keymap("n", "<M-k>", ":resize +2<CR>", opts)
+keymap("n", "<M-h>", ":vertical resize -2<CR>", opts)
+keymap("n", "<M-l>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<C-l>", ":bnext<CR>", opts)
+keymap("n", "<C-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "\|", ":m .-2<CR>", opts)
+keymap("n", "\\", ":m .+1<CR>", opts)
+
+-- Useful binding
+keymap("n", "<leader>o", "o<esc>^C", opts)
+keymap("n", "<leader>O", "O<esc>^C", opts)
+keymap("n", "<leader>n", "nzz", opts)
+keymap("n", "<leader>N", "Nzz", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("n", "\|", ":m '<-2<CR>gv", opts)
+keymap("n", "\\", ":m '>+1<CR>gv", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Command Mode --
+-- Better command line filtering
+keymap("c", "<C-n>", "<Down>", opts)
+keymap("c", "<C-p>", "<Up>", opts)

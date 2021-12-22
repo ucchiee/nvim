828919243c3be857a5c51eb44ebb76ad2d5644f5
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 20:59:35 +0900

    cmp.lua: init

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ require "user.options"  -- source lua/user/options.lua
 require "user.keymaps"  -- source lua/user/keymaps.lua
 require "user.plugins"  -- source lua/user/plugins.lua
 require "user.colorscheme"  -- source lua/user/colorscheme.lua
+require "user.cmp"  -- source lua/user/cmp.lua

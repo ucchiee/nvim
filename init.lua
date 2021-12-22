f3c5e8b4cd2986b5ed897dff49602b65ec738f61
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 23:52:42 +0900

    init.lua: enabled floaterm

--- init.lua
+++ init.lua
@@ -5,3 +5,4 @@ require "user.colorscheme"  -- source lua/user/colorscheme.lua
 require "user.cmp"  -- source lua/user/cmp.lua
 require "user.lsp"  -- source lua/user/lsp/init.lua
 require "user.telescope"  -- source lua/user/telescope.lua
+require "user.floaterm"  --source lua/user/floaterm.lua

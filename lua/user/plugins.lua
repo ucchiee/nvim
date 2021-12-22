bee34e1d6a1fa187137b077813ecda71f29ead64
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-22 20:02:04 +0900

    plugins.lua: init

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,11 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  -- colorscheme
+  use "lunarvim/colorschemes"
+  -- use "lunarvim/darkplus.nvim"
+  use "folke/tokyonight.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

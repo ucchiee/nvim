2434eb7df26fdc2df9d5daf77a47f2244dfab7f2
Yusuke Uchida <51429010+ucchiee@users.noreply.github.com>
2021-12-25 11:33:21 +0900

    Refactor/2112251132 (#22)
    * plugins.lua: deleted spaces
    
    * keymaps.lua: added comment

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -46,7 +46,7 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff
-  
+
   -- colorscheme
   use "lunarvim/colorschemes"
   -- use "lunarvim/darkplus.nvim"

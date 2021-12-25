f300e5c30b630571b677211135f088c0165717c1
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-25 10:55:26 +0900

    init.lua,plugins.lua,treesitter.lua: initialized treesitter

--- init.lua
+++ init.lua
@@ -6,3 +6,4 @@ require "user.cmp"  -- source lua/user/cmp.lua
 require "user.lsp"  -- source lua/user/lsp/init.lua
 require "user.telescope"  -- source lua/user/telescope.lua
 require "user.floaterm"  --source lua/user/floaterm.lua
+require "user.treesitter"  -- source lua/user/treesitter.lua

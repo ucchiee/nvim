41845e6d23c63a91d434494a08e5e911a9c35ab8
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-26 13:38:07 +0900

    options.lua: configured netrw

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -45,6 +45,12 @@ vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 vim.cmd [[
-	set nocp                    " 'compatible' is not set
-	filetype plugin on          " plugins are enabled
+  set nocp                    " 'compatible' is not set
+  filetype plugin on          " plugins are enabled
+  let g:netrw_banner=0        " disable annoying banner
+  let g:netrw_browse_split=4  " open in prior window
+  let g:netrw_altv=1          " open splits to the right
+  let g:netrw_liststyle=3     " tree view
+  let g:netrw_list_hide=netrw_gitignore#Hide()
+  let g:netrw_list_hide.=',\(^\|\s\s\)\zs\.\S\+'
 ]]

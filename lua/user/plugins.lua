4b2a130029cacaf9157d220bbc3165d97063d4bf
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-29 12:34:45 +0900

    feat: added colorshceme, kanagawa

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,6 +55,7 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes"
   -- use "lunarvim/darkplus.nvim"
   use "folke/tokyonight.nvim"
+  use "rebelot/kanagawa.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin

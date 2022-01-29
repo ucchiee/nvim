e28fb2550183f6f381c464e7c4cdff2c0b370709
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-29 23:09:04 +0900

    feat: null-ls.lua: configured isort, flake8

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,13 +8,24 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
-    formatting.stylua,
-    diagnostics.flake8,
-  },
+local black_config = { "--fast", "-l", "120" }
+local flake8_config = {
+	"--max-line-length",
+	"120",
+	"--ignore=E402,E203,E501,W503",
+	"--exclude=__init__.py",
 }
+local isort_config = { "--stdout", "--profile", "black", "-" }
+
+null_ls.setup({
+	debug = false,
+	sources = {
+    -- formatter
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = black_config }),
+		formatting.isort.with({ extra_args = isort_config }),
+		formatting.stylua,
+    -- linter
+		diagnostics.flake8.with({ extra_args = flake8_config }),
+	},
+})

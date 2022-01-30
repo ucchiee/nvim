682aecc7aab5895998c1e4adaf2166728dcc1ab4
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-30 22:13:43 +0900

    feat: filetype.lua: added autocommand

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("user.options") -- source lua/user/options.lua
+require("user.filetype") -- source lua/user/filetype.lua
 require("user.keymaps") -- source lua/user/keymaps.lua
 require("user.plugins") -- source lua/user/plugins.lua
 require("user.colorscheme") -- source lua/user/colorscheme.lua

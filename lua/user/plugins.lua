865c4ad0dc81a9dc3d4e73899c2b062b80ec14f6
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-02-05 17:33:20 +0900

    feat: colorscheme.lua: changed to onenord

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -68,6 +68,7 @@ return packer.startup(function(use)
 	-- use "lunarvim/darkplus.nvim"
 	use("folke/tokyonight.nvim")
 	use("rebelot/kanagawa.nvim")
+  use 'rmehri01/onenord.nvim'
 
 	-- cmp plugins
 	use("hrsh7th/nvim-cmp") -- The completion plugin

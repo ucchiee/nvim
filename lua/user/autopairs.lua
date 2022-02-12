82a187b8e11690a91c66e0e228e74b8f5bc8e32b
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-02-13 00:00:00 +0900

    feat: autopairs.lua: configured autopairs

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -5,6 +5,8 @@ if not status_ok then
 end
 
 npairs.setup({
+  map_c_h = true,  -- Map the <C-h> key to delete a pair
+  map_c_w = true, -- map <c-w> to delete a pair if possible
 	check_ts = true,
 	ts_config = {
 		lua = { "string", "source" },
@@ -24,6 +26,33 @@ npairs.setup({
 		highlight_grey = "LineNr",
 	},
 })
+
+local Rule = require("nvim-autopairs.rule")
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']')
+}
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")

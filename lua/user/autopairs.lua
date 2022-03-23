475177afa837f8acb8b397cf6c04a91dde4d9766
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-03-23 16:00:25 +0900

    feat: autopairs.lua: commented out special rule

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -27,32 +27,32 @@ npairs.setup({
 	},
 })
 
-local Rule = require("nvim-autopairs.rule")
-npairs.add_rules {
-  Rule(' ', ' ')
-    :with_pair(function (opts)
-      local pair = opts.line:sub(opts.col - 1, opts.col)
-      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-    end),
-  Rule('( ', ' )')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%)') ~= nil
-      end)
-      :use_key(')'),
-  Rule('{ ', ' }')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%}') ~= nil
-      end)
-      :use_key('}'),
-  Rule('[ ', ' ]')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%]') ~= nil
-      end)
-      :use_key(']')
-}
+-- local Rule = require("nvim-autopairs.rule")
+-- npairs.add_rules {
+--   Rule(' ', ' ')
+--     :with_pair(function (opts)
+--       local pair = opts.line:sub(opts.col - 1, opts.col)
+--       return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+--     end),
+--   Rule('( ', ' )')
+--       :with_pair(function() return false end)
+--       :with_move(function(opts)
+--           return opts.prev_char:match('.%)') ~= nil
+--       end)
+--       :use_key(')'),
+--   Rule('{ ', ' }')
+--       :with_pair(function() return false end)
+--       :with_move(function(opts)
+--           return opts.prev_char:match('.%}') ~= nil
+--       end)
+--       :use_key('}'),
+--   Rule('[ ', ' ]')
+--       :with_pair(function() return false end)
+--       :with_move(function(opts)
+--           return opts.prev_char:match('.%]') ~= nil
+--       end)
+--       :use_key(']')
+-- }
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")

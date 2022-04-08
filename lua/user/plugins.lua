a808648d6f54ca2e1f18d7dc5d31f2b643de89fd
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-04-08 15:00:55 +0900

    feat: plugings.lua: added markdown preview plugin, but cannot install correctly

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -115,6 +115,13 @@ return packer.startup(function(use)
 	-- Git
 	use("lewis6991/gitsigns.nvim")
 
+	-- markdown, cannot install this plugin for some reasons.
+	-- use({
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	ft = "markdown",
+	-- 	run = "cd app && yarn install",
+	-- })
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

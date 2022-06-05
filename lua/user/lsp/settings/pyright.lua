ba722fbc692ed9c343d086de9233d99ebeb93daf
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-06-05 17:38:48 +0900

    pyright.lua: added config for extrapaths

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -3,6 +3,7 @@ return {
 		python = {
 			analysis = {
 				typeCheckingMode = "off",
+				extraPaths = { "/Users/uu/.asdf/installs/python/3.9.5/lib/python3.9/site-packages" }, -- need to fix (do not use constant)
 			},
 		},
 	},

af2d3d7e629153cccef42170955eaf6eb23425f6
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-07-06 14:47:56 +0900

    plugins.lua: formatted

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,7 +44,8 @@ return packer.startup(function(use)
 	use("wbthomason/packer.nvim") -- Have packer manage itself
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+	-- use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+	use("jiangmiao/auto-pairs")
 	use("windwp/nvim-ts-autotag") -- AutoTag, integrates with treesitter
 	use("numToStr/Comment.nvim") -- Easily comment stuff
 	use("kyazdani42/nvim-web-devicons")
@@ -123,7 +124,7 @@ return packer.startup(function(use)
 
 	-- Git
 	use("lewis6991/gitsigns.nvim")
-  use("tpope/vim-fugitive")
+	use("tpope/vim-fugitive")
 
 	-- markdown, cannot install this plugin for some reasons.
 	-- use({

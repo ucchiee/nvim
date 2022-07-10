4567090d941cc1a316d194f2e708bf064a517362
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-07-10 09:58:44 +0900

    changed to use nvim-autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,8 +44,7 @@ return packer.startup(function(use)
 	use("wbthomason/packer.nvim") -- Have packer manage itself
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	-- use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("jiangmiao/auto-pairs")
+	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
 	use("windwp/nvim-ts-autotag") -- AutoTag, integrates with treesitter
 	use("numToStr/Comment.nvim") -- Easily comment stuff
 	use("kyazdani42/nvim-web-devicons")

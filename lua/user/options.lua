7446d7854af23071095ca857cc122c6df83ace4d
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-07-10 10:04:06 +0900

    disabled scrolloff, sidescrolloff

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -30,8 +30,8 @@ local options = {
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
+  -- scrolloff = 8,                           -- is one of my fav
+  -- sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
 }
 

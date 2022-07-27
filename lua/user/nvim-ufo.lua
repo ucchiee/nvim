9644152df99e0263b633d20d94ddd6c191c99906
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-07-27 09:47:13 +0900

    ufo: changed separater

--- lua/user/nvim-ufo.lua
+++ lua/user/nvim-ufo.lua
@@ -3,7 +3,7 @@ if not ok then
 	return
 end
 
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+-- vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99

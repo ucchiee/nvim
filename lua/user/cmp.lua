b99148052eb62102d7779f1a25f67cb8cb63543e
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-08-02 05:26:24 +0900

    nvim-cmp: configured lsp-kind

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -8,6 +8,11 @@ if not snip_status_ok then
 	return
 end
 
+local lsp_kind_ok, lspkind = pcall(require, "lspkind")
+if not lsp_kind_ok then
+	return
+end
+
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local has_words_before = function()
@@ -15,35 +20,6 @@ local has_words_before = function()
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
---   פּ ﯟ   some other good icons
-local kind_icons = {
-	Text = "  ",
-	Method = "m  ",
-	Function = "  ",
-	Constructor = "  ",
-	Field = "  ",
-	Variable = "  ",
-	Class = "  ",
-	Interface = "  ",
-	Module = "  ",
-	Property = "  ",
-	Unit = "  ",
-	Value = "  ",
-	Enum = "  ",
-	Keyword = "  ",
-	Snippet = "  ",
-	Color = "  ",
-	File = "  ",
-	Reference = "  ",
-	Folder = "  ",
-	EnumMember = "  ",
-	Constant = "  ",
-	Struct = "  ",
-	Event = "  ",
-	Operator = "  ",
-	TypeParameter = "  ",
-}
--- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup({
 	snippet = {
@@ -93,22 +69,7 @@ cmp.setup({
 		}),
 	},
 	formatting = {
-		fields = { "abbr", "kind", "menu" },
-		format = function(entry, vim_item)
-			local max_abbr_len = 80
-			if string.len(vim_item.abbr) > max_abbr_len then
-				vim_item.abbr = string.sub(vim_item.abbr, 1, max_abbr_len) .. "..."
-			end
-			vim_item.kind = (kind_icons[vim_item.kind] or "") .. vim_item.kind
-			vim_item.menu = ({
-				nvim_lua = "[NVIM_LUA]",
-				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
-				path = "[Path]",
-			})[entry.source.name]
-			return vim_item
-		end,
+		format = lspkind.cmp_format({ maxwidth = 80 }),
 	},
 	sources = {
 		{ name = "nvim_lsp" }, -- listed in this order

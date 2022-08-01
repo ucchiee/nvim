732cc49cf980a1e658b4df603c20490864c7df6e
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-08-02 05:27:22 +0900

    lsp-kind: installed lsp-kind

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -103,6 +103,7 @@ return packer.startup(function(use)
 	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
 	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
 	use("folke/lsp-colors.nvim")
+	use("onsails/lspkind.nvim")
 	use("j-hui/fidget.nvim")
 	use({
 		"rmagatti/goto-preview",

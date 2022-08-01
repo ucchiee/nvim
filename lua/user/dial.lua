66e3ca4a5dcb568bd92c1173e388e4c7bb9b5af2
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-08-01 14:47:13 +0900

    dial: updated dial configuration

--- lua/user/dial.lua
+++ lua/user/dial.lua
@@ -1,9 +1,13 @@
-vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), {noremap = true})
-vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), {noremap = true})
-vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), {noremap = true})
-vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), {noremap = true})
-vim.keymap.set("v", "g<C-a>", require("dial.map").inc_gvisual(), {noremap = true})
-vim.keymap.set("v", "g<C-x>", require("dial.map").dec_gvisual(), {noremap = true})
+local ok, dial_map = pcall(require, "dial.map")
+if not ok then
+  return
+end
+vim.keymap.set("n", "<C-a>", dial_map.inc_normal(), {noremap = true})
+vim.keymap.set("n", "<C-x>", dial_map.dec_normal(), {noremap = true})
+vim.keymap.set("v", "<C-a>", dial_map.inc_visual(), {noremap = true})
+vim.keymap.set("v", "<C-x>", dial_map.dec_visual(), {noremap = true})
+vim.keymap.set("v", "g<C-a>", dial_map.inc_gvisual(), {noremap = true})
+vim.keymap.set("v", "g<C-x>", dial_map.dec_gvisual(), {noremap = true})
 
 -- Visit the url below to configure additional augend
 -- https://github.com/monaqa/dial.nvim

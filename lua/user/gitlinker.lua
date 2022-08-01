a99120fb251197b4b593d5693c1adcda676d4c54
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-08-02 05:26:34 +0900

    gitlinker.lua: deleted mappings

--- lua/user/gitlinker.lua
+++ lua/user/gitlinker.lua
@@ -4,6 +4,3 @@ if not ok then
 end
 
 gitlinker.setup()
-
-vim.api.nvim_set_keymap('n', '<leader>gy', '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>', {desc="Get link"})
--- vim.keymap.set('v', '<leader>gy', '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>', {})

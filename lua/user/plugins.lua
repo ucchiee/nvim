03cb84e651ad22034bbc5af639126776b51c96c5
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-01 03:59:05 +0900

    plugins.lua: added max_jobs option

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -38,152 +38,157 @@ packer.init({
 })
 
 -- Install your plugins here
-return packer.startup(function(use)
-	-- My plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("windwp/nvim-ts-autotag") -- AutoTag, integrates with treesitter
-	use("numToStr/Comment.nvim") -- Easily comment stuff
-	use("kyazdani42/nvim-web-devicons")
-	use("kyazdani42/nvim-tree.lua")
-	use("akinsho/bufferline.nvim")
-	use("moll/vim-bbye")
-	use("lewis6991/impatient.nvim")
-	use("ahmedkhalf/project.nvim")
-	use("goolord/alpha-nvim")
-	use("folke/which-key.nvim")
-	use("norcalli/nvim-colorizer.lua")
-	use("kana/vim-textobj-user")
-	use("kana/vim-textobj-entire")
-	use("lukas-reineke/indent-blankline.nvim")
-	-- use("max397574/better-escape.nvim")
+return packer.startup({
+	function(use)
+		-- My plugins here
+		use("wbthomason/packer.nvim") -- Have packer manage itself
+		use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+		use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+		use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+		use("windwp/nvim-ts-autotag") -- AutoTag, integrates with treesitter
+		use("numToStr/Comment.nvim") -- Easily comment stuff
+		use("kyazdani42/nvim-web-devicons")
+		use("kyazdani42/nvim-tree.lua")
+		use("akinsho/bufferline.nvim")
+		use("moll/vim-bbye")
+		use("lewis6991/impatient.nvim")
+		use("ahmedkhalf/project.nvim")
+		use("goolord/alpha-nvim")
+		use("folke/which-key.nvim")
+		use("norcalli/nvim-colorizer.lua")
+		use("kana/vim-textobj-user")
+		use("kana/vim-textobj-entire")
+		use("lukas-reineke/indent-blankline.nvim")
+		-- use("max397574/better-escape.nvim")
 
-	-- debug adapter protocol
-	use({ "mfussenegger/nvim-dap" })
-	use({ "rcarriga/nvim-dap-ui" })
+		-- debug adapter protocol
+		use({ "mfussenegger/nvim-dap" })
+		use({ "rcarriga/nvim-dap-ui" })
 
-	-- status line
-	use("nvim-lualine/lualine.nvim")
-	use({
-		"SmiteshP/nvim-gps",
-		requires = "nvim-treesitter/nvim-treesitter",
-	})
+		-- status line
+		use("nvim-lualine/lualine.nvim")
+		use({
+			"SmiteshP/nvim-gps",
+			requires = "nvim-treesitter/nvim-treesitter",
+		})
 
-	-- tpope plugins
-	use("tpope/vim-surround")
-	use("tpope/vim-repeat")
+		-- tpope plugins
+		use("tpope/vim-surround")
+		use("tpope/vim-repeat")
 
-	-- colorscheme
-	-- use "lunarvim/darkplus.nvim"
-	-- use("folke/tokyonight.nvim")
-	-- use("rebelot/kanagawa.nvim")
-	use("rmehri01/onenord.nvim")
-	use("sainnhe/gruvbox-material")
+		-- colorscheme
+		-- use "lunarvim/darkplus.nvim"
+		-- use("folke/tokyonight.nvim")
+		-- use("rebelot/kanagawa.nvim")
+		use("rmehri01/onenord.nvim")
+		use("sainnhe/gruvbox-material")
 
-	-- cmp plugins
-	use("hrsh7th/nvim-cmp") -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("hrsh7th/cmp-nvim-lua") -- nvim api completion
-	use("hrsh7th/cmp-nvim-lsp") -- lsp
-	-- use("hrsh7th/cmp-cmdline-history") -- command line
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("onsails/lspkind.nvim")
+		-- cmp plugins
+		use("hrsh7th/nvim-cmp") -- The completion plugin
+		use("hrsh7th/cmp-buffer") -- buffer completions
+		use("hrsh7th/cmp-path") -- path completions
+		use("hrsh7th/cmp-cmdline") -- cmdline completions
+		use("hrsh7th/cmp-nvim-lua") -- nvim api completion
+		use("hrsh7th/cmp-nvim-lsp") -- lsp
+		-- use("hrsh7th/cmp-cmdline-history") -- command line
+		use("saadparwaiz1/cmp_luasnip") -- snippet completions
+		use("onsails/lspkind.nvim")
 
-	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
-	use({ "ucchiee/vscode-hugo-snippets", branch = "feat/ucchiee-template" }) -- hugo snippet
+		-- snippets
+		use("L3MON4D3/LuaSnip") --snippet engine
+		use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+		use({ "ucchiee/vscode-hugo-snippets", branch = "feat/ucchiee-template" }) -- hugo snippet
 
-	-- LSP
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig") -- enable LSP
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
-	use("folke/lsp-colors.nvim")
-	use("j-hui/fidget.nvim")
-	use("glepnir/lspsaga.nvim")
-	use("ray-x/lsp_signature.nvim")
-	use("folke/trouble.nvim")
-	use({
-		"rmagatti/goto-preview",
-		config = function()
-			require("goto-preview").setup({})
-		end,
-	})
+		-- LSP
+		use("williamboman/mason.nvim")
+		use("williamboman/mason-lspconfig.nvim")
+		use("neovim/nvim-lspconfig") -- enable LSP
+		use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+		use("folke/lsp-colors.nvim")
+		use("j-hui/fidget.nvim")
+		use("glepnir/lspsaga.nvim")
+		use("ray-x/lsp_signature.nvim")
+		use("folke/trouble.nvim")
+		use({
+			"rmagatti/goto-preview",
+			config = function()
+				require("goto-preview").setup({})
+			end,
+		})
 
-	-- Telescope
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-media-files.nvim")
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use({
-		"nvim-telescope/telescope-frecency.nvim",
-		requires = { "tami5/sqlite.lua" },
-	})
+		-- Telescope
+		use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } })
+		use("nvim-telescope/telescope-media-files.nvim")
+		use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+		use({
+			"nvim-telescope/telescope-frecency.nvim",
+			requires = { "tami5/sqlite.lua" },
+		})
 
-	-- FTerm
-	use("voldikss/vim-floaterm")
+		-- FTerm
+		use("voldikss/vim-floaterm")
 
-	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-	use("lewis6991/spellsitter.nvim")
+		-- Treesitter
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+		})
+		use("JoosepAlviste/nvim-ts-context-commentstring")
+		use("lewis6991/spellsitter.nvim")
 
-	-- Git
-	use("lewis6991/gitsigns.nvim")
-	use("tpope/vim-fugitive")
-	use({
-		"ruifm/gitlinker.nvim",
-		requires = "nvim-lua/plenary.nvim",
-	})
+		-- Git
+		use("lewis6991/gitsigns.nvim")
+		use("tpope/vim-fugitive")
+		use({
+			"ruifm/gitlinker.nvim",
+			requires = "nvim-lua/plenary.nvim",
+		})
 
-	-- markdown
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
-	use("AckslD/nvim-FeMaco.lua")
+		-- markdown
+		use({
+			"iamcco/markdown-preview.nvim",
+			run = function()
+				vim.fn["mkdp#util#install"]()
+			end,
+		})
+		use("AckslD/nvim-FeMaco.lua")
 
-	-- Folding
-	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+		-- Folding
+		use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
 
-	-- Scrolling
-	-- use("karb94/neoscroll.nvim")
+		-- Scrolling
+		-- use("karb94/neoscroll.nvim")
 
-	-- syntax tree surfer
-	use("ziontee113/syntax-tree-surfer")
+		-- syntax tree surfer
+		use("ziontee113/syntax-tree-surfer")
 
-	-- symbol
-	use("simrat39/symbols-outline.nvim")
+		-- symbol
+		use("simrat39/symbols-outline.nvim")
 
-	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-	})
-	use("sindrets/winshift.nvim")
-	use("monaqa/dial.nvim")
-	use({ "kevinhwang91/nvim-hlslens" })
-	use("petertriho/nvim-scrollbar")
-	use("andymass/vim-matchup")
-	use({
-		"danymat/neogen",
-		requires = "nvim-treesitter/nvim-treesitter",
-		tag = "*",
-	})
-	use("zdcthomas/medit")
-	use("haya14busa/vim-asterisk")
+		use({
+			"folke/todo-comments.nvim",
+			requires = "nvim-lua/plenary.nvim",
+		})
+		use("sindrets/winshift.nvim")
+		use("monaqa/dial.nvim")
+		use({ "kevinhwang91/nvim-hlslens" })
+		use("petertriho/nvim-scrollbar")
+		use("andymass/vim-matchup")
+		use({
+			"danymat/neogen",
+			requires = "nvim-treesitter/nvim-treesitter",
+			tag = "*",
+		})
+		use("zdcthomas/medit")
+		use("haya14busa/vim-asterisk")
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+		-- Automatically set up your configuration after cloning packer.nvim
+		-- Put this at the end after all plugins
+		if PACKER_BOOTSTRAP then
+			require("packer").sync()
+		end
+	end,
+	config = {
+		max_jobs = 10,
+	},
+})

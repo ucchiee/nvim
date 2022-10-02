1809bcdcddb537d8bf4bccfade7da32f42920b49
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-02 12:38:34 +0900

    scrollbar.lua: confired hlslens

--- lua/user/scrollbar.lua
+++ lua/user/scrollbar.lua
@@ -1,11 +1,12 @@
+---@diagnostic disable: redefined-local
 local ok, scrollbar = pcall(require, "scrollbar")
 if not ok then
 	return
 end
 scrollbar.setup({
-	handle = {
-		color = "#7a6f6f",
-	},
+	--[[ handle = { ]]
+	--[[ 	color = "#7a6f6f", ]]
+	--[[ }, ]]
 	marks = {
 		Search = { color = "#e78a4e" },
 		Error = { color = "#ea6962" },
@@ -13,4 +14,15 @@ scrollbar.setup({
 	},
 })
 
-require("scrollbar.handlers.search").setup()
+local ok, hlslens = pcall(require, "hlslens")
+if not ok then
+	vim.notify("hlslens cannot be loaded")
+	return
+end
+hlslens.setup({
+	calm_down = false,
+	nearest_only = true,
+	nearest_float_when = "auto",
+})
+
+--[[ require("scrollbar.handlers.search").setup() ]]

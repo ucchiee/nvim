e35e73a7b1a5c769a18b96066b18343c77329ea4
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-04 13:52:08 +0900

    lualine.lua: updated

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -37,6 +37,7 @@ local filetype = {
   "filetype",
   icons_enabled = true,
   icon = nil,
+  padding = 0,
 }
 
 local branch = {
@@ -95,6 +96,41 @@ local navic_status = {
   cond = navic.is_available,
 }
 
+local mixed_indent = {
+  function()
+    local space_pat = [[\v^ +]]
+    local tab_pat = [[\v^\t+]]
+    local space_indent = vim.fn.search(space_pat, 'nwc')
+    local tab_indent = vim.fn.search(tab_pat, 'nwc')
+    local mixed = (space_indent > 0 and tab_indent > 0)
+    local mixed_same_line
+    if not mixed then
+      mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], 'nwc')
+      mixed = mixed_same_line > 0
+    end
+    if not mixed then return '' end
+    if mixed_same_line ~= nil and mixed_same_line > 0 then
+      return 'MI:' .. mixed_same_line
+    end
+    local space_indent_cnt = vim.fn.searchcount({ pattern = space_pat, max_count = 1e3 }).total
+    local tab_indent_cnt = vim.fn.searchcount({ pattern = tab_pat, max_count = 1e3 }).total
+    if space_indent_cnt > tab_indent_cnt then
+      return 'MI:' .. tab_indent
+    else
+      return 'MI:' .. space_indent
+    end
+  end,
+  color = { fg = "#ffffff", gui = "bold" }
+}
+
+local trailing_white = {
+  function()
+    local space = vim.fn.search([[\s\+$]], 'nwc')
+    return space ~= 0 and "TW:" .. space or ""
+  end,
+  color = { fg = "#ffffff", gui = "bold" }
+}
+
 lualine.setup({
   options = {
     icons_enabled = true,
@@ -112,6 +148,8 @@ lualine.setup({
     -- lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_x = {
       diagnostics,
+      trailing_white,
+      mixed_indent,
       { "encoding", padding = { left = 1, right = 0 } },
       {
         "fileformat",

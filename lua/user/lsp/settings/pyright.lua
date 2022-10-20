283d730ac0074a10f5a11f30466fd3c2a2a5691b
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-20 20:09:22 +0900

    pyright.lua: added robustness

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,9 +1,13 @@
+local home_dir = os.getenv("HOME")
 return {
 	settings = {
 		python = {
 			analysis = {
 				typeCheckingMode = "off",
-				extraPaths = { "/Users/uu/.asdf/installs/python/3.9.5/lib/python3.9/site-packages" }, -- need to fix (do not use constant)
+				extraPaths = {
+					home_dir .. "/.asdf/installs/python/3.9.5/lib/python3.9/site-packages",
+					home_dir .. "/.asdf/installs/python/3.9.12/lib/python3.9/site-packages",
+				}, -- need to fix (do not use constant)
 			},
 		},
 	},

5ea730ec0cea052a2e8c97c0084e88c106658c02
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-10-22 18:23:42 +0900

    dap-ui.lua: changed not to close dap-ui on event_terminated or event_exited

--- lua/user/dap/dap-ui.lua
+++ lua/user/dap/dap-ui.lua
@@ -39,9 +39,9 @@ dapui.setup({
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-	dapui.close()
-end
+--[[ dap.listeners.before.event_terminated["dapui_config"] = function() ]]
+--[[ 	dapui.close() ]]
+--[[ end ]]
+--[[ dap.listeners.before.event_exited["dapui_config"] = function() ]]
+--[[ 	dapui.close() ]]
+--[[ end ]]

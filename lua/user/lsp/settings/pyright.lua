daf56147b047d5cfdd30b70cd9e15f0541542fb0
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-01-24 00:28:10 +0900

    pyright.lua: updated to python 3.11

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -5,7 +5,7 @@ return {
 			analysis = {
 				typeCheckingMode = "off",
 				extraPaths = {
-					home_dir .. "/.asdf/installs/python/3.9.5/lib/python3.9/site-packages",
+					home_dir .. "/.asdf/installs/python/3.11.1/lib/python3.11/site-packages",
 					home_dir .. "/.asdf/installs/python/3.9.12/lib/python3.9/site-packages",
 				}, -- need to fix (do not use constant)
 			},

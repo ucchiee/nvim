06893278992d35d42fb0a38840bd857df3687b66
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-22 12:32:00 +0900

    osc52.lua: changed function name

--- lua/user/osc52.lua
+++ lua/user/osc52.lua
@@ -15,10 +15,10 @@ vim.keymap.set("n", "<leader>c", require("osc52").copy_operator, { expr = true }
 vim.keymap.set("n", "<leader>cc", "<leader>c_", { remap = true })
 vim.keymap.set("v", "<leader>c", require("osc52").copy_visual)
 
-function copy()
+function COPY()
 	if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
 		require("osc52").copy_register("+")
 	end
 end
 
-vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+vim.api.nvim_create_autocmd("TextYankPost", { callback = COPY })

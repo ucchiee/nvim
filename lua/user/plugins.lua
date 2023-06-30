f0555542da3edba39874dcb4fc0b1b679ddb40a5
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-06-30 15:16:30 +0900

    plugins.lua: deleted packer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -14,7 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Install your plugins here
 require("lazy").setup({
 	-- My plugins here
-	"wbthomason/packer.nvim", -- Have packer manage itself
 	"lewis6991/impatient.nvim",
 	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
 	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins

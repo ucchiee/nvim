2c40d4323d6d3327b17ef0df86cae70345158449
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-12-30 13:51:36 +0900

    osc52.lua: tmux_passthrough

--- lua/user/osc52.lua
+++ lua/user/osc52.lua
@@ -9,6 +9,7 @@ osc52.setup({
 	max_length = 0, -- Maximum length of selection (0 for no limit)
 	silent = false, -- Disable message on successful copy
 	trim = false, -- Trim surrounding whitespaces before copy
+  tmux_passthrough = true
 })
 
 vim.keymap.set("n", "<leader>c", require("osc52").copy_operator, { expr = true })

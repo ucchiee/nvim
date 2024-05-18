18ffc53bb04d04b953ead600cbb2bfe621964234
Yusuke Uchida <fantomyuu0623@gmail.com>
2024-05-18 23:20:56 +0900

    options.lua: enabled inccommand option

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -17,7 +17,7 @@ local options = {
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
   termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
@@ -25,7 +25,7 @@ local options = {
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = false,                      -- highlight the current line
-  number = false,                           -- set numbered lines
+  number = false,                          -- set numbered lines
   relativenumber = false,                  -- set relative numbered lines
   numberwidth = 2,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
@@ -33,6 +33,7 @@ local options = {
   scrolloff = 10,                          -- is one of my fav
   sidescrolloff = 0,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  inccommand = "split",
 }
 
 vim.opt.shortmess:append("c")
